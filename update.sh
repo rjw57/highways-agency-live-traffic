3cd1c4d37bc5acd1edfd6cb30cae6d2af9912690
Rich Wareham <rjw57@cam.ac.uk>
2013-04-20 14:57:05 +0100

    remove useless fetching of metadata

--- update.sh
+++ update.sh
@@ -23,7 +23,6 @@ function die() {
 for ds in $DATASETS; do
 	echo "Downloading dataset $ds"
 	mkdir --p "$ds"
-	$CURL -s "${URLBASE}/${ds}/metadata.xml" > "${ds}/metadata.xml" || die "Failed to downliad $ds metadata"
 	$CURL -s "${URLBASE}/${ds}/content" > "${ds}/content.xml" || die "Failed to download $ds content"
 done
 
